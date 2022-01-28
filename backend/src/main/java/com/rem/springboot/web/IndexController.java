f2e298eabdfe6adc9aa9b524ded9fbc4b27dd616
hremmus <hremmus@gmail.com>
2022-01-28 23:43:27 +0900

    Index Application for RESTful Web Service

---
+++ backend/src/main/java/com/rem/springboot/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.rem.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+  @GetMapping("/")
+  public String index() {
+    return "Grettings from Spring Boot!";
+  }
+}
