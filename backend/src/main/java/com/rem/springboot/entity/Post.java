44024d7cb5c077e49335df42fa10f5dbc70ade43
hremmus <hremmus@gmail.com>
2022-02-15 23:22:09 +0900

    Added PostUpdateRequest
    추가되는 이미지는 MultipartFile로 받고, 삭제되어야 하는 이미지는 id만 받아 FileService에서 이를 처리

--- backend/src/main/java/com/rem/springboot/entity/Post.java
+++ backend/src/main/java/com/rem/springboot/entity/Post.java
@@ -1,7 +1,9 @@
 package com.rem.springboot.entity;
 
+import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +14,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import org.springframework.web.multipart.MultipartFile;
+import com.rem.springboot.payload.request.PostUpdateRequest;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -55,5 +60,43 @@ public class Post extends EntityDate {
       images.add(i);
       i.initPost(this);
     });
+  }
+
+  private void deleteImages(List<Image> deleted) {
+    deleted.stream().forEach(di -> images.remove(di));
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public static class ImageUpdateResult {
+    private List<MultipartFile> addedImageFiles;
+    private List<Image> addedImages;
+    private List<Image> deletedImages;
+  }
+
+  public ImageUpdateResult update(PostUpdateRequest request) {
+    title = request.getTitle();
+    content = request.getContent();
+    List<Image> addedImages = convertImageFilesToImages(request.getAddedImages());
+    List<Image> deletedImages = convertImageIdsToImages(request.getDeletedImages());
+    addImages(addedImages);
+    deleteImages(deletedImages);
+    return new ImageUpdateResult(request.getAddedImages(), addedImages, deletedImages);
+  }
+
+  private List<Image> convertImageFilesToImages(List<MultipartFile> imageFiles) {
+    return imageFiles.stream().map(imageFile -> new Image(imageFile.getOriginalFilename())).collect(toList());
+  }
+
+  private List<Image> convertImageIdsToImages(List<Long> ids) {
+    return ids.stream()
+        .map(id -> convertImageIdToImage(id))
+        .filter(i -> i.isPresent())
+        .map(i -> i.get())
+        .collect(toList());
+  }
+
+  private Optional<Image> convertImageIdToImage(Long id) {
+    return images.stream().filter(i -> i.getId().equals(id)).findAny();
   }
 }
