7d1113e81bc95c8d825603970b754412e4a7d190
hremmus <hremmus@gmail.com>
2022-02-23 23:38:06 +0900

    Added Deleting Comment

--- backend/src/main/java/com/rem/springboot/repository/CommentRepository.java
+++ backend/src/main/java/com/rem/springboot/repository/CommentRepository.java
@@ -9,7 +9,7 @@ import com.rem.springboot.entity.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
   @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.parent WHERE c.id = :id")
-  Optional<Comment> findByIdIfParentPresent(@Param("id") Long id);
+  Optional<Comment> findByIdWithParent(@Param("id") Long id);
 
   @Query("SELECT c FROM Comment c JOIN FETCH c.user LEFT JOIN FETCH c.parent WHERE c.post.id = :postId ORDER BY c.parent.id ASC NULLS FIRST, c.id ASC")
   List<Comment> findAllWithUserAndParentByPostIdOrderByParentIdAscNullsFirstCommentIdAsc(@Param("postId") Long postId);
