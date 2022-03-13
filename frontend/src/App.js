58e40fcc2e78fd4180e9dd07bdf49787b9f63fe8
hremmus <hremmus@gmail.com>
2022-03-13 23:45:01 +0900

    refactor: Replace class component with function component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,36 +2,33 @@ import HeaderContainer from "containers/HeaderContainer";
 import storage from "lib/storage";
 import Auth from "pages/Auth";
 import Home from "pages/Home";
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import * as userActions from "redux/modules/user";
 
-class App extends Component {
-  initializeUserInfo = async () => {
-    const loggedInfo = storage.get("loggedInfo"); // 로그인 정보를 로컬스토리지에서 가져옵니다.
-    if (!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
+function App() {
+  useEffect(() => {
+    const initializeUserInfo = async () => {
+      const loggedInfo = storage.get("loggedInfo"); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+      if (!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
 
-    const { UserActions } = this.props;
-    UserActions.setLoggedInfo(loggedInfo);
-  };
+      const { UserActions } = this.props;
+      UserActions.setLoggedInfo(loggedInfo);
+    };
+    initializeUserInfo();
+  });
 
-  componentDidMount() {
-    this.initializeUserInfo();
-  }
-
-  render() {
-    return (
-      <div>
-        <HeaderContainer />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth/*" element={<Auth />} />
-        </Routes>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <HeaderContainer />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/auth/*" element={<Auth />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default connect(null, (dispatch) => ({
