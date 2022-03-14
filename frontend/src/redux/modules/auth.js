b3a4a728f5445dd397a3799e20987c2d43863670
hremmus <hremmus@gmail.com>
2022-03-14 23:44:30 +0900

    feat: Connect Logout API
    - redux-persist 라이브러리 추가
    - local storage에 user 관련 state를 저장하여 새로고침해도 로그인 유지
    - redux-toolkit + redux-persist 적용 위해 store 생성 코드에 serializableCheck-ignoredActions 설정 추가
    - 리덕스 모듈의 state를 사용하는 과정에서 발생하는 'A non-serializable value was detected ~' 오류를 제거하기 위해 Map 제거
    - Join/Login Form을 초기화하는 initializeForm 액션 추가
    - connect 함수 대신 useSelector, useDispatch Hooks를 사용

--- frontend/src/redux/modules/auth.js
+++ frontend/src/redux/modules/auth.js
@@ -1,42 +1,50 @@
-import { Map } from "immutable";
+import produce from "immer";
 import { createAction, handleActions } from "redux-actions";
 
+const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
 const CHANGE_INPUT = "auth/CHANGE_INPUT";
 const SET_ERROR = "auth/SET_ERROR";
 
-export const changeInput = createAction(CHANGE_INPUT);
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
+export const changeInput = createAction(
+  CHANGE_INPUT,
+  ({ form, key, value }) => ({
+    form, // join, login
+    key, // username, password, passwordConfirm, ...
+    value, // 실제 바꾸려는 값
+  })
+);
 export const setError = createAction(SET_ERROR);
 
-const initialState = Map({
-  join: Map({
-    form: Map({
-      email: "",
-      password: "",
-      passwordConfirm: "",
-      nickname: "",
-    }),
-    error: null,
-  }),
-  login: Map({
-    form: Map({
-      email: "",
-      password: "",
-    }),
-    error: null,
-  }),
-  result: Map({}),
-});
+const initialState = {
+  join: {
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    nickname: "",
+  },
+  login: {
+    email: "",
+    password: "",
+  },
+  authError: null,
+};
 
 export default handleActions(
   {
-    [CHANGE_INPUT]: (state, action) => {
-      const { form, name, value } = action.payload;
-      return state.setIn([form, "form", name], value);
-    },
-    [SET_ERROR]: (state, action) => {
-      const { form, message } = action.payload;
-      return state.setIn([form, "error"], message);
-    },
+    [CHANGE_INPUT]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+      authError: null,
+    }),
+    [SET_ERROR]: (state, { payload: message }) => ({
+      ...state,
+      authError: message,
+    }),
   },
   initialState
 );
