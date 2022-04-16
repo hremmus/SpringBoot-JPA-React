f0bed3311ae5c821627787ac3b76a1b1875aa0bf
hremmus <hremmus@gmail.com>
2022-04-16 23:47:49 +0900

    feat: Add Creating Replies Comments Component
    - content에 토글 연결 => 일반 댓글과 공유되지 않도록 common과 reply 객체에 content를 각각 관리
    - 모든 CommentItem 객체에 작동하지 않도록 id와 boolean 값을 쌍으로 하는 shownReplyInput 추가
    - CommentReplyContainer 추가 => 리스트에서 parentId를 넘겨줌
    - 계층형 구조 구현을 위해 서버에 리스트 재요청 => convertOneArray, calcDepth 함수 리듀서에서 호출

--- frontend/src/redux/modules/comment.js
+++ frontend/src/redux/modules/comment.js
@@ -1,21 +1,33 @@
+import { produce } from "immer";
 import { createAction, handleActions } from "redux-actions";
 
 const LOAD_COMMENTS = "comment/LOAD_COMMENTS";
 const CHANGE_INPUT = "comment/CHANGE_INPUT";
 const SET_COMMENT = "comment/SET_COMMENT";
 const UNLOAD_COMMENT = "comment/UNLOAD_COMMENT";
+const TOGGLE_SWITCH = "comment/TOGGLE_SWITCH";
+const CLEAN_REPLY_COMMENT = "comment/CLEAN_REPLY_COMMENT";
 
 export const unloadComment = createAction(UNLOAD_COMMENT);
 export const loadComments = createAction(LOAD_COMMENTS);
-export const changeInput = createAction(CHANGE_INPUT, ({ key, value }) => ({
-  key,
-  value,
-}));
+export const changeInput = createAction(
+  CHANGE_INPUT,
+  ({ input, key, value }) => ({
+    input,
+    key,
+    value,
+  })
+);
 export const setComment = createAction(SET_COMMENT);
+export const toggleSwitch = createAction(TOGGLE_SWITCH, (id) => id);
+export const cleanReplyComment = createAction(CLEAN_REPLY_COMMENT);
 
 const initialState = {
   comments: [],
-  content: "",
+  common: { content: "" },
+  reply: { content: "" },
+  parentId: null,
+  shownReplyInput: {},
 };
 
 const comment = handleActions(
@@ -23,18 +35,73 @@ const comment = handleActions(
     [UNLOAD_COMMENT]: () => {},
     [LOAD_COMMENTS]: (state, action) => ({
       ...state,
-      comments: action.payload,
+      comments: calcDepth(convertOneArray(action.payload)),
     }),
-    [CHANGE_INPUT]: (state, { payload: { key, value } }) => ({
-      ...state,
-      [key]: value,
-    }),
+    [CHANGE_INPUT]: (state, { payload: { input, key, value } }) =>
+      produce(state, (draft) => {
+        draft[input][key] = value;
+      }),
     [SET_COMMENT]: (state, action) => ({
+      ...state,
       comments: [...state.comments, action.payload],
-      content: "",
+      common: { content: "" },
+    }),
+    [TOGGLE_SWITCH]: (state, { payload: id }) => ({
+      ...state,
+      shownReplyInput: {
+        [id]: !state.shownReplyInput[id], // 댓글을 클릭하면 해당 댓글의 id를 key, boolean 값을 value로 하는 값이 객체에 추가됨. 존재하는 key면 value만 변경됨(true <=> false)
+      },
+    }),
+    [CLEAN_REPLY_COMMENT]: (state, action) => ({
+      ...state,
+      parentId: null,
+      reply: { content: "" },
+      shownReplyInput: {},
     }),
   },
   initialState
 );
+
+// 계층형 구조의 배열을 전부 꺼내어 한 배열에 담음
+const convertOneArray = (array) => {
+  const newArray = [];
+  const recursion = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      array[i].childrenIds = [];
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      newArray.push(array[i]);
+
+      if (array[i].children.length !== 0) {
+        array[i].childrenIds = array[i].children.map((child) => child.id); // id만 꺼냄
+        recursion(array[i].children);
+      }
+    }
+  };
+
+  recursion(array);
+  return newArray;
+};
+
+// 부모 객체의 개수만큼 자식 객체의 depth에 담음
+const calcDepth = (array) => {
+  const newArray = [...array]; // readonly로 depth 프로퍼티를 추가할 수 없음 => 얕은 복사
+  for (let i = 0; i < newArray.length; i++) {
+    newArray[i].depth = 0;
+  }
+
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = i + 1; j < newArray.length; j++) {
+      if (
+        newArray[i].childrenIds.length !== 0 && // 최상위 댓글 제외
+        newArray[i].childrenIds.includes(newArray[j].id) // JAVA의 contains
+      ) {
+        newArray[j].depth = newArray[i].depth + 1; // 부모 depth에 +1
+      }
+    }
+  }
+  return newArray;
+};
 
 export default comment;
