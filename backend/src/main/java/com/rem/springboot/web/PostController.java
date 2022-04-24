6d2805e640b6de02e1f442b77b3ff563da00cdc0
hremmus <hremmus@gmail.com>
2022-04-24 23:48:29 +0900

    feat: Add image upload when creating post
    - Controller에서 PostDto를 받을 때 @RequestBody로는 Binary Stream이 포함된 MultipartFile을 받을 수 없으므로 @ModelAttribute로 변경

--- backend/src/main/java/com/rem/springboot/web/PostController.java
+++ backend/src/main/java/com/rem/springboot/web/PostController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,7 +34,7 @@ public class PostController {
   @PostMapping("/posts")
   @ResponseStatus(HttpStatus.CREATED)
   @AssignUserId
-  public Response create(@Valid @RequestBody PostCreateRequest request) {
+  public Response create(@Valid @ModelAttribute PostCreateRequest request) {
     return Response.success(postService.create(request));
   }
 
