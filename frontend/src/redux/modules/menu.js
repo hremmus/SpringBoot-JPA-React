35aa1356c4d8e739705ff699b0cde3a557a1d1cb
hremmus <hremmus@gmail.com>
2022-06-04 23:40:48 +0900

    refactor: Apply category depth in posts
    - 재귀호출을 함에도 자식의 자식 카테고리까지 도달하지 않던 문제 수정
    - 여러 페이지에서 사용되는 menuData들을 한 객체로 묶음

--- frontend/src/redux/modules/menu.js
+++ frontend/src/redux/modules/menu.js
@@ -1,19 +1,24 @@
 import { createAction, handleActions } from "redux-actions";
 
-export const menuData = [
-  { name: "talk", link: "/posts" },
-  { name: "forecast", link: "/location/yangyang", state: "양양" },
-  { name: "shop", link: "/" },
-];
-
-export const locationMenuData = [
-  { name: "양양 yangyang", link: "/location/yangyang", state: "양양" },
-  { name: "제주 jeju", link: "/location/jeju", state: "제주" },
-  { name: "고성 goseong", link: "/location/goseong", state: "고성" },
-  { name: "강릉 gangneung", link: "/location/gangeung", state: "강릉" },
-  { name: "포항 pohang", link: "/location/pohang", state: "포항" },
-  { name: "남해 namhae", link: "/location/namhae", state: "남해" },
-];
+export const menuData = {
+  initial: [
+    { name: "talk", link: "/posts" },
+    { name: "forecast", link: "/location/yangyang", state: "양양" },
+    { name: "shop", link: "/" },
+  ],
+  location: [
+    { name: "양양 yangyang", link: "/location/yangyang", state: "양양" },
+    { name: "제주 jeju", link: "/location/jeju", state: "제주" },
+    { name: "고성 goseong", link: "/location/goseong", state: "고성" },
+    { name: "강릉 gangneung", link: "/location/gangeung", state: "강릉" },
+    { name: "포항 pohang", link: "/location/pohang", state: "포항" },
+    { name: "남해 namhae", link: "/location/namhae", state: "남해" },
+  ],
+  admin: [
+    { name: "회원 관리", link: "/admin/users" },
+    { name: "카테고리 관리", link: "/admin/categories" },
+  ],
+};
 
 const SET_MENU = "menu/SET_MENU";
 const INITIALIZE = "menu/INITIALIZE";
@@ -22,7 +27,7 @@ export const setMenu = createAction(SET_MENU);
 export const initialize = createAction(INITIALIZE);
 
 const initialState = {
-  menu: menuData,
+  menu: menuData.initial,
 };
 
 const menu = handleActions(
