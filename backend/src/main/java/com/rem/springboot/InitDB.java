8afe5368c47c34e7cceb328abbf18df11545f927
hremmus <hremmus@gmail.com>
2024-10-22 04:50:04 +0900

    feat: Add conditional execution to initDB
    - DB가 비어있을 떄만 init 실행

--- backend/src/main/java/com/rem/springboot/InitDB.java
+++ backend/src/main/java/com/rem/springboot/InitDB.java
@@ -55,65 +55,73 @@ public class InitDB {
   }
 
   private void initRole() {
-    roleRepository.saveAll(List.of(new Role(ERole.ROLE_USER), new Role(ERole.ROLE_ADMIN)));
+    if (roleRepository.count() == 0) roleRepository.saveAll(List.of(new Role(ERole.ROLE_USER), new Role(ERole.ROLE_ADMIN)));
   }
 
   private void initTestUser() throws RoleNotFoundException {
-    userRepository.saveAll(List.of(
-        new User(user1Email, passwordEncoder.encode(password), "nickname1", List.of(
-            roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new))),
-        new User(user2Email, passwordEncoder.encode(password), "nickname2", List.of(
-            roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new))),
-        new User(adminEmail, passwordEncoder.encode(password), "admin", List.of(
-            roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new),
-            roleRepository.findByName(ERole.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new)))));
+    if (userRepository.count() == 0) {
+      userRepository.saveAll(List.of(
+          new User(user1Email, passwordEncoder.encode(password), "nickname1", List.of(
+              roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new))),
+          new User(user2Email, passwordEncoder.encode(password), "nickname2", List.of(
+              roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new))),
+          new User(adminEmail, passwordEncoder.encode(password), "admin", List.of(
+              roleRepository.findByName(ERole.ROLE_USER).orElseThrow(RoleNotFoundException::new),
+              roleRepository.findByName(ERole.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new)))));
+    }
   }
 
   private void initCategory() {
-    Category c1 = categoryRepository.save(new Category("category1", null));
-    Category c2 = categoryRepository.save(new Category("category2", c1));
-    Category c3 = categoryRepository.save(new Category("category3", c1));
-    Category c4 = categoryRepository.save(new Category("category4", c2));
-    Category c5 = categoryRepository.save(new Category("category5", c2));
-    Category c6 = categoryRepository.save(new Category("category6", c4));
-    Category c7 = categoryRepository.save(new Category("category7", c3));
-    Category c8 = categoryRepository.save(new Category("category8", null));
+    if (categoryRepository.count() == 0) {
+      Category c1 = categoryRepository.save(new Category("category1", null));
+      Category c2 = categoryRepository.save(new Category("category2", c1));
+      Category c3 = categoryRepository.save(new Category("category3", c1));
+      Category c4 = categoryRepository.save(new Category("category4", c2));
+      Category c5 = categoryRepository.save(new Category("category5", c2));
+      Category c6 = categoryRepository.save(new Category("category6", c4));
+      Category c7 = categoryRepository.save(new Category("category7", c3));
+      Category c8 = categoryRepository.save(new Category("category8", null));
+    }
   }
 
   private void initPost() {
-    User user1 = userRepository.findAll().get(0);
-    User user2 = userRepository.findAll().get(1);
-    Category category1 = categoryRepository.findAll().get(0);
-    Category category2 = categoryRepository.findAll().get(1);
-    Category category3 = categoryRepository.findAll().get(2);
-    IntStream.range(0, 25)
-    .forEach(i -> postRepository.save(
-        new Post("title" + i, "content" + i, user1, category1, List.of())));
-    IntStream.range(0, 25)
-    .forEach(i -> postRepository.save(
-        new Post("title" + i, "content" + i, user1, category2, List.of())));
-    IntStream.range(0, 25)
-    .forEach(i -> postRepository.save(
-        new Post("title" + i, "content" + i, user1, category3, List.of())));
-    IntStream.range(0, 25)
-    .forEach(i -> postRepository.save(
-        new Post("title" + i, "content" + i, user2, category1, List.of())));
-    IntStream.range(0, 25)
-    .forEach(i -> postRepository.save(
-        new Post("title" + i, "content" + i, user2, category2, List.of())));
+    if (postRepository.count() == 0) {
+      User user1 = userRepository.findAll().get(0);
+      User user2 = userRepository.findAll().get(1);
+      Category category1 = categoryRepository.findAll().get(0);
+      Category category2 = categoryRepository.findAll().get(1);
+      Category category3 = categoryRepository.findAll().get(2);
+      IntStream.range(0, 25)
+      .forEach(i -> postRepository.save(
+          new Post("title" + i, "content" + i, user1, category1, List.of())));
+      IntStream.range(0, 25)
+      .forEach(i -> postRepository.save(
+          new Post("title" + i, "content" + i, user1, category2, List.of())));
+      IntStream.range(0, 25)
+      .forEach(i -> postRepository.save(
+          new Post("title" + i, "content" + i, user1, category3, List.of())));
+      IntStream.range(0, 25)
+      .forEach(i -> postRepository.save(
+          new Post("title" + i, "content" + i, user2, category1, List.of())));
+      IntStream.range(0, 25)
+      .forEach(i -> postRepository.save(
+          new Post("title" + i, "content" + i, user2, category2, List.of())));
+    }
   }
 
   private void initComment() {
-    User user = userRepository.findAll().get(0);
-    Post post = postRepository.findAll().get(0);
-    Comment comment1 = commentRepository.save(new Comment("content", user, post, null));
-    Comment comment2 = commentRepository.save(new Comment("content", user, post, comment1));
-    Comment comment3 = commentRepository.save(new Comment("content", user, post, comment1));
-    Comment comment4 = commentRepository.save(new Comment("content", user, post, comment2));
-    Comment comment5 = commentRepository.save(new Comment("content", user, post, comment2));
-    Comment comment6 = commentRepository.save(new Comment("content", user, post, comment4));
-    Comment comment7 = commentRepository.save(new Comment("content", user, post, comment3));
-    Comment comment8 = commentRepository.save(new Comment("content", user, post, null));
+    if (commentRepository.count() == 0) {
+      User user = userRepository.findAll().get(0);
+      Post post = postRepository.findAll().get(0);
+      Comment comment1 = commentRepository.save(new Comment("content", user, post, null));
+      Comment comment2 = commentRepository.save(new Comment("content", user, post, comment1));
+      Comment comment3 = commentRepository.save(new Comment("content", user, post, comment1));
+      Comment comment4 = commentRepository.save(new Comment("content", user, post, comment2));
+      Comment comment5 = commentRepository.save(new Comment("content", user, post, comment2));
+      Comment comment6 = commentRepository.save(new Comment("content", user, post, comment4));
+      Comment comment7 = commentRepository.save(new Comment("content", user, post, comment3));
+      Comment comment8 = commentRepository.save(new Comment("content", user, post, null));
+    }
   }
 
   public String getUser1Email() {
